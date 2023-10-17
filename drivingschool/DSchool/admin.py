673dfa986b9c031e5a43b1fd59bb96da5ada3d4a
C2Chandelier <102954303+C2Chandelier@users.noreply.github.com>
2023-10-17 15:28:41 +0200

    up

--- drivingschool/DSchool/admin.py
+++ drivingschool/DSchool/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Student, Instructor, Planning
+
+class PlanningAdmin(admin.ModelAdmin):
+    list_display = ["student", "instructor", "date", "place", "hour"]
+    list_filter = ["date"]
+    search_fields = ["student", "instructor", "date"]
+
+admin.site.register(Student)
+admin.site.register(Instructor)
+admin.site.register(Planning, PlanningAdmin)
 
 # Register your models here.
--- drivingschool/drivingschool/settings.py
+++ drivingschool/drivingschool/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "DSchool.apps.DschoolConfig",
     "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
